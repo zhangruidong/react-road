208be7a9b916b9daedeeea65a88cd439a11129e4
zhangruidong <zzzruidong@gmail.com>
2018-03-08 15:35:55 +0800

    react ==> 好友列表及动画完成

--- 011-friend-list/src/index.js
+++ 011-friend-list/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './style.css'
 import DATA from './data.js';
 
+/*
+* Element.scrollHeight 这个只读属性是一个元素内容高度的度量，包括由于溢出导致的视图中不可见内容。没有垂直滚动条的情况下，scrollHeight值与元素视图填充所有内容所需要的最小值clientHeight相同。包括元素的padding，但不包括元素的border和margin。scrollHeight也包括 ::before 和 ::after这样的伪元素。
+* */
+
 class Item extends React.Component {
   constructor(props) {
     super(props);
   }
-  
   render() {
     return (
         <li>
@@ -24,6 +27,21 @@ class Group extends React.Component {
   constructor(props) {
     super(props);
   };
+  animateShow(isShow) {
+    let list = this.refs.list;
+    if(isShow){
+      list.style.height = list.scrollHeight +'px';
+    }else{
+      list.style.height = '0'
+    }
+  };
+  shouldComponentUpdate(nextProps) {
+    this.animateShow(nextProps.show);
+    return true;
+  };
+  componentDidMount() {
+    this.animateShow(this.props.show);
+  }
   handleClick = () => {
     this.props.changeShow(this.props.index);
   };
@@ -31,7 +49,7 @@ class Group extends React.Component {
     return (
         <div className={'group'}>
           <h2 className={'title'} onClick={this.handleClick}>{this.props.data.name}</h2>
-          <ul style={ {display: this.props.show?'block':'none'} }>
+          <ul ref={'list'} className={'list hide'}>
             {this.props.data.list.map( (item,index) => <Item key={index} data={item}/> )}
           </ul>
         </div>
@@ -45,7 +63,6 @@ class Panel extends React.Component {
     this.state = {show:[true,false,false]}
   }
   changeShow = (index) => {
-    console.log(index);
     if(this.state.show[index]){ //打开
       this.state.show[index] = false;
       this.setState( {show:[...this.state.show]} )
